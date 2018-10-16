e7f45ddb8e202bbf2eb6d16a59ba626499d47e46
Navyasri <ganga.navyasri@capgemini.com>
2018-10-16 07:45:03 -0400

    first

---
+++ src/main/java/com/capgemini/service/ServiceApplication.java
@@ -0,0 +1,31 @@
+package com.capgemini.service;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class ServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceApplication.class, args);
+	}
+	
+	@RefreshScope
+	@RestController
+	class Message{
+		@Value("${message:hello}")
+		private String message;
+			
+		@RequestMapping("/message")
+		String getMessage() {
+			return this.message;
+		}
+		
+		}
+}
